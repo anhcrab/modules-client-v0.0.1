790209c1bc1ecbceaef2d9deb595ab30a742cde8
User <anhcrafter@gmail.com>
2023-09-14 06:52:28 +0700

    update props-types

--- src/pages/Admin/Type.jsx
+++ src/pages/Admin/Type.jsx
@@ -1,81 +1,97 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axiosClient from "../../axios-client.js";
-import {Link} from "react-router-dom";
-import {useStateContext} from "../../context/ContextProvider.jsx";
+import { Link } from "react-router-dom";
+import { useStateContext } from "../../context/ContextProvider.jsx";
 
 export default function Type() {
   const [types, settypes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const {setNotification} = useStateContext()
+  const { setNotification } = useStateContext();
 
   useEffect(() => {
     gettypes();
-  }, [])
+  }, []);
 
-  const onDeleteClick = user => {
+  const onDeleteClick = (user) => {
     if (!window.confirm("Are you sure you want to delete this user?")) {
-      return
+      return;
     }
-    axiosClient.delete(`/types/${user.id}`)
-      .then(() => {
-        setNotification('User was successfully deleted')
-        gettypes()
-      })
-  }
+    axiosClient.delete(`/types/${user.id}`).then(() => {
+      setNotification("User was successfully deleted");
+      gettypes();
+    });
+  };
 
   const gettypes = () => {
-    setLoading(true)
-    axiosClient.get('/types')
+    setLoading(true);
+    axiosClient
+      .get("/types")
       .then(({ data }) => {
-        setLoading(false)
-        settypes(data)
+        setLoading(false);
+        settypes(data);
       })
       .catch(() => {
-        setLoading(false)
-      })
-  }
+        setLoading(false);
+      });
+  };
 
   return (
     <div>
-      <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <h1>types</h1>
-        <Link className="btn-add" to="/product-type/new">Add new</Link>
+        <Link className="btn-add" to="/product-type/new">
+          Add new
+        </Link>
       </div>
       <div className="card animated fadeInDown">
         <table>
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          {loading &&
-            <tbody>
             <tr>
-              <td colSpan="5" class="text-center">
-                Loading...
-              </td>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Actions</th>
             </tr>
-            </tbody>
-          }
-          {!loading &&
+          </thead>
+          {loading && (
             <tbody>
-            {types && types.map(u => (
-              <tr key={u.id}>
-                <td>{u.id}</td>
-                <td>{u.name}</td>
-                <td>
-                  <Link className="btn-edit" to={'/product-type/' + u.id}>Edit</Link>
-                  &nbsp;
-                  <button className="btn-delete" onClick={ev => onDeleteClick(u)}>Delete</button>
+              <tr>
+                <td colSpan="5" class="text-center">
+                  Loading...
                 </td>
               </tr>
-            ))}
             </tbody>
-          }
+          )}
+          {!loading && (
+            <tbody>
+              {types &&
+                types.map((u) => (
+                  <tr key={u.id}>
+                    <td>{u.id}</td>
+                    <td>{u.name}</td>
+                    <td>
+                      <Link className="btn-edit" to={"/product-type/" + u.id}>
+                        Edit
+                      </Link>
+                      &nbsp;
+                      <button
+                        className="btn-delete"
+                        onClick={(ev) => onDeleteClick(u)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          )}
         </table>
       </div>
     </div>
-  )
+  );
 }
